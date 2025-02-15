c538cf2072490cbe55cac5a91dd76880ae97c982
Giacomo202210029 <u202210029@upc.edu.pe>
2025-02-15 15:28:25 -0500

    feat:  Added models/task.go

--- internal/models/task.go
+++ internal/models/task.go
@@ -1,1 +1,13 @@
 package models
+
+type Task struct {
+	ID        int    `json:"id"`
+	Title     string `json:"title"`
+	Completed bool   `json:"completed"`
+}
+
+//struct es una coleccion de campos
+
+//¿para que es el type?
+//Para definir un nuevo tipo de dato
+//Task es un nuevo tipo de dato que es un struct
