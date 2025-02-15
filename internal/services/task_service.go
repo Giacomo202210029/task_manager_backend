7edc1b94a76fb6b0a2dac60071284124bee18a36
Giacomo202210029 <u202210029@upc.edu.pe>
2025-02-15 15:29:07 -0500

    feat:  Added services

--- internal/services/task_service.go
+++ internal/services/task_service.go
@@ -1,1 +1,14 @@
 package services
+
+import (
+	"Goland/internal/models"
+	"Goland/internal/repositories"
+)
+
+func GetAllTasks() []models.Task {
+	return repositories.GetAllTasks()
+}
+
+func AddTask(task models.Task) models.Task {
+	return repositories.CreateTask(task)
+}
