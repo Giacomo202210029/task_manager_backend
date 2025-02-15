c6d3ef9fa4f2b065c6698f563c545c6ab3e00631
Giacomo202210029 <u202210029@upc.edu.pe>
2025-02-15 15:28:17 -0500

    feat:  Added router.go

--- internal/routes/router.go
+++ internal/routes/router.go
@@ -1,1 +1,39 @@
 package routes
+
+//entonces esto va siempre arriba
+
+//nuestras importaciones
+import (
+	"Goland/internal/controllers"
+	"github.com/gorilla/mux"
+)
+
+func SetupRouter() *mux.Router {
+	router := mux.NewRouter()
+
+	//¿Que es mux?
+	//Es un enrutador HTTP y
+	//un envoltorio para la solicitud y respuesta de http.
+
+	//¿Que es NewRouter?
+	//Es una función que devuelve un nuevo enrutador.
+
+	//¿Que es *mux.Router?
+	//Es un enrutador HTTP y un envoltorio para la solicitud y respuesta de http.
+
+	//¿Que es ese *?
+	//Es un puntero, que es una dirección de memoria de una variable.
+
+	//¿Que es := ?
+	//Es una declaración corta de variable.
+	//Se puede usar para declarar e
+	//inicializar variables locales.
+
+	//aqui se asignan las rutas
+	//se asigna la ruta /tasks con el metodo GET a la funcion GetTasks
+	router.HandleFunc("/tasks", controllers.GetTasks).Methods("GET")
+	router.HandleFunc("/tasks", controllers.CreateTask).Methods("POST")
+
+	// Puedes agregar más rutas para actualizar y eliminar tareas
+	return router
+}
