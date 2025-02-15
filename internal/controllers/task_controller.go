de3e71d530ccd8f6af4049660e191d6847cd8e1c
Giacomo202210029 <u202210029@upc.edu.pe>
2025-02-15 15:28:44 -0500

    feat:  Added controller/task_controller.go

--- internal/controllers/task_controller.go
+++ internal/controllers/task_controller.go
@@ -1,1 +1,26 @@
 package controllers
+
+import (
+	"Goland/internal/models"
+	"Goland/internal/services"
+	"encoding/json"
+	"net/http"
+)
+
+func getTasks(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	tasks := services.GetAllTasks()
+	json.NewEncoder(w).Encode(tasks)
+}
+
+func CreateTask(w http.ResponseWriter, r *http.Request) {
+	var task models.Task
+	err := json.NewDecoder(r.Body).Decode(&task)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	newTask := services.AddTask(task)
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(newTask)
+}
